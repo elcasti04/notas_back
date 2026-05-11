6f585e30a8c3a64c349c3386a7abf80dc23c3859
AndresCastro <188876553+elcasti04@users.noreply.github.com>
2026-05-11 08:54:51 -0500

    Agregando nuevas secciones

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import routes from '../routes/index.routes.js'
 import errorHandler from '../middlewares/errorHandler.js'
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+}));
 app.use(express.json())
 app.use('/', routes)
 
